b835949dfaf4a635f5e6cacd13748f5bd62ffb24
João Almeida <176084410+04ramalmeida@users.noreply.github.com>
2024-09-08 22:09:03 +0100

    Add readings entity

---
+++ CETWebProject/Data/DataContext.cs
@@ -0,0 +1,16 @@
+﻿
+
+using CETWebProject.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CETWebProject.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Reading> MonthlyReadings { get; set; }
+    }
+}
---
+++ CETWebProject/Data/Entities/Reading.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Routing.Constraints;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CETWebProject.Data.Entities
+{
+    public class Reading
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Amount of consumed water")]
+        [DisplayFormat(DataFormatString = "{0:C2}", ApplyFormatInEditMode = false)]
+        public double usageAmount { get; set; }
+
+        [Display(Name = "Reading date")]
+        public DateTime dataDeLeitura { get; set; }
+    }
+}
